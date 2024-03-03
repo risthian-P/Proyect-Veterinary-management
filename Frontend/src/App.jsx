6e167d6e7ad3abe0f932c6818091fb82af979625
risthian-P <cristhian.panora@gmail.com>
2024-03-03 16:15:57 -0500

    Pagina login, context, provider

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -14,37 +14,41 @@ import Actualizar from './paginas/Actualizar'
 import Perfil from './paginas/Perfil'
 import { Confirmar } from './paginas/Confirmar'
 import Restablecer from './paginas/Restablecer'
+import { AuthProvider } from './context/AuthProvider'
 
 
 function App() {
   return (
     <>
     <BrowserRouter>
-      <Routes>
-        
-        <Route index element={<LandinPage/>}/>
+      <AuthProvider>
 
-        <Route path='/' element={<Auth/>}>
-          <Route path='login' element={<Login/>}/>
-          <Route path='register' element={<Register/>}/>
-          <Route path='forgot/:id' element={<Forgot/>}/>
-          <Route path='confirmar/:token' element={<Confirmar/>}/>
-          <Route path='recuperar-password/:token' element={<Restablecer/>}/>
-          <Route path='*' element={<NotFound />} />
-        </Route>
+          <Routes>
+            
+            <Route index element={<LandinPage/>}/>
 
-        <Route path='/dashboard' element={<Dashboard/>}>
-          <Route index element={<Perfil/>}/>
-          <Route path='listar' element={<Listar/>}/>
-          <Route path='visualizar/:id' element={<Visualizar/>}/>
-          <Route path='crear' element={<Crear/>}/>
-          <Route path='actualizar/:id' element={<Actualizar/>}/>
-        </Route>
+            <Route path='/' element={<Auth/>}>
+              <Route path='login' element={<Login/>}/>
+              <Route path='register' element={<Register/>}/>
+              <Route path='forgot/:id' element={<Forgot/>}/>
+              <Route path='confirmar/:token' element={<Confirmar/>}/>
+              <Route path='recuperar-password/:token' element={<Restablecer/>}/>
+              <Route path='*' element={<NotFound />} />
+            </Route>
+
+            <Route path='/dashboard' element={<Dashboard/>}>
+              <Route index element={<Perfil/>}/>
+              <Route path='listar' element={<Listar/>}/>
+              <Route path='visualizar/:id' element={<Visualizar/>}/>
+              <Route path='crear' element={<Crear/>}/>
+              <Route path='actualizar/:id' element={<Actualizar/>}/>
+            </Route>
 
 
 
 
-      </Routes>
+          </Routes>
+      </AuthProvider>
     </BrowserRouter>
     </>
   )
