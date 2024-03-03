6c6b72e93d75b3302232f3cd6bc41af2bbe65c59
risthian-P <cristhian.panora@gmail.com>
2024-03-03 18:10:02 -0500

    Módulo Veterinario - Perfil, CardPerfil, Password

--- Frontend/src/paginas/Perfil.jsx
+++ Frontend/src/paginas/Perfil.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { CardPerfil } from '../componets/Perfil/CardPerfil'
 import FormularioPerfil from '../componets/Perfil/FormularioPerfil'
+import Password from '../componets/Perfil/Password'
+
 
 const Perfil = () => {
     return (
@@ -17,6 +19,7 @@ const Perfil = () => {
                 </div>
                 <div className='w-full md:w-1/2'>
                     <CardPerfil/>
+                    <Password/>
                 </div>
             </div>
         </>
